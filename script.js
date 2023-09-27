7b196c813c24d1d4b5bbbc563cfd6b34f214d20e
Anisah91 <137075663+Anisah91@users.noreply.github.com>
2023-09-27 17:50:10 +0200

    added day 6

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class = "row">`;
   days = ["Wednesday", "Thursday", "Friday", "Saturday"];
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         ` 
